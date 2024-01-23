2c66b9fc72beefb9a1eededa2ced046a51185660
Zachary-Kaplan <143014811+Zachary-Kaplan@users.noreply.github.com>
2024-01-23 10:10:04 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,9 +37,10 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
+  String sNew = toLowercase(word);
   for(int i =0; i < word.length() / 2; i++)
     {
-      if(word.charAt(i) != word.charAt(word.length() - (i + 1)))
+      if(sNew.charAt(i) != word.charAt(sNew.length() - (i + 1)))
       {
         return false;
       }
